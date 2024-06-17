df69b7a9551da4698daea11bdd9d84198f26200d
Puneet kumar <puneetkumar041@gmail.com>
2024-06-16 23:35:29 -0700

    Create name.py

---
+++ Day1/name.py
@@ -0,0 +1,33 @@
+######## strings and changing case 
+
+name="ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+######## Combining or Concatenating Strings 
+
+first_name="ada"
+last_name="lovelace"
+full_name=first_name +" "+ last_name
+print(full_name)
+
+
+print("Hello, "+full_name.title()+" !")
+
+
+message="Hello, "+full_name.title()+" !"
+print(message)
+
+#Adding Whitespace to Strings with Tabs or Newlines
+#To add a tab use  \t
+print("Python")
+
+#To add a tab use  \t
+print("\tPython")
+
+#To add a new line use  \n
+print("\nPython")
+
+#combine tabs and newlines in a single string.
+print("Languages:\n\t\tPython\n\t\tC\n\t\tJava")
